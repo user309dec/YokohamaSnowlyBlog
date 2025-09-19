a9733f5066a9cc1ed71631781f1335c03c321c9b
user309dec <yl328@rice.edu>
2025-09-19 15:17:49 -0500

    🚧

--- astro.config.ts
+++ astro.config.ts
@@ -25,7 +25,8 @@ import config from './src/site.config.ts'
 // https://astro.build/config
 export default defineConfig({
   // Top-Level Options
-  site: 'https://astro-pure.js.org',
+  site: 'https://user309dec.github.io',
+  base: '/YokohamaSnowlyBlog',
   // Deploy to a sub path; See https://astro-pure.js.org/docs/setup/deployment#platform-with-base-path
   // base: '/astro-pure/',
   trailingSlash: 'never',
